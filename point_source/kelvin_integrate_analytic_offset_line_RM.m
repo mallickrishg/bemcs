0b2a3de4d3096b377435db7fa6e810f82368854d
Rishav Mallick <mallickrishg@gmail.com>
2023-07-23 12:11:30 -0700

    can evaluate function exactly ON the source

--- point_source/kelvin_integrate_analytic_offset_line_RM.m
+++ point_source/kelvin_integrate_analytic_offset_line_RM.m
@@ -30,31 +30,56 @@ ux_definite = int(ux, x0, [-1.0 1.0]);
 ux_function_handle = matlabFunction(ux_definite);
 
 %% Plot result over a grid
-n_pts = 501;
+n_pts = 1001;
 x_vec = linspace(-2, 2, n_pts);
 y_vec = linspace(-1.5, 1.5, n_pts);
-[x_mat, y_mat] = meshgrid(x_vec, y_vec);
+
+eval_type = 0;% 0 - line, 1 - xy grid
+if eval_type == 1
+    % create a box grid of points
+    [x_mat, y_mat] = meshgrid(x_vec, y_vec);
+else
+    % only evluate function along a line at y=0
+    x_mat = x_vec;
+    y_mat = x_vec.*0;
+end
+
 sxx_mat = zeros(size(x_mat));
 ux_mat = zeros(size(x_mat));
 
 tic
-for i=1:n_pts
-    for j=1:n_pts
-        % sxx_mat(i, j) = sxx_function_handle(1, 0, 0.25, x_mat(i, j), y_mat(i, j), 0);
-        ux_mat(i, j) = ux_function_handle(1, 0, 1, 0.25, x_mat(i, j), y_mat(i, j), 0);
+if eval_type == 1
+    for i=1:n_pts
+        for j=1:n_pts
+            % sxx_mat(i, j) = sxx_function_handle(1, 0, 0.25, x_mat(i, j), y_mat(i, j), 0);
+            ux_mat(i, j) = ux_function_handle(1, 0, 1, 0.25, x_mat(i, j), 0, y_mat(i, j));
+        end
+    end
+else
+    for i = 1:n_pts
+        ux_mat(i) = ux_function_handle(1, 0, 1, 0.25, x_mat(i), 0, y_mat(i));
     end
 end
+
 toc
 
 % plot results
 figure(1),clf
-contourf(x_mat, y_mat, ux_mat)
-colorbar;
-clim([-1,1].*max(abs(ux_mat(:))))
-axis("equal")
-colormap(parula(40))
-title("analytical solution")
-
+if eval_type == 1
+    contourf(x_mat, y_mat, ux_mat)
+    colorbar;
+    clim([-1,1].*max(abs(ux_mat(:))))
+    axis("equal")
+    colormap(parula(40))
+    title("analytical solution")
+else
+    plot(x_mat,ux_mat,'-','LineWidth',3), hold on
+    plot([-1,1],[0,0],'k-','Linewidth',2)
+    axis tight, grid on
+    xlabel('x'), ylabel('f(x)')
+    ylim([-1,1].*max(abs(ux_mat(:)))*1.1)
+    title("analytical solution")
+end
 %% numerical integration (GL quadrature)
 
 ux_numeric_mat_total = zeros(size(x_mat));
@@ -62,7 +87,7 @@ ux_numeric_mat = zeros(size(x_mat));
 % point_vec = linspace(-1.0, 1.0, 100);
 y0 = 0;
 
-N_gl = 39;
+N_gl = 15;
 [xk,wk] = calc_gausslegendre_weights(N_gl);
 
 tic
@@ -74,34 +99,44 @@ end
 toc
 
 figure(2),clf
+if eval_type==1
+    subplot(3,1,1)
+    contourf(x_mat, y_mat, ux_mat)
+    colorbar;
+    clim([-1,1].*max(abs(ux_mat(:))))
+    axis("equal")
+    colormap(parula(10))
+    title("analytical solution")
+    set(gca,'Fontsize',12)
 
-subplot(3,1,1)
-contourf(x_mat, y_mat, ux_mat)
-colorbar;
-clim([-1,1].*max(abs(ux_mat(:))))
-axis("equal")
-colormap(parula(10))
-title("analytical solution")
-set(gca,'Fontsize',12)
+    subplot(3,1,2)
+    contourf(x_mat, y_mat, ux_numeric_mat_total)
+    colorbar;
+    clim([-1,1].*max(abs(ux_mat(:))))
+    axis("equal")
+    colormap(parula(10))
+    title(['Gauss-Legendre numerical integration of order ' num2str(N_gl)])
+    set(gca,'Fontsize',12)
 
-subplot(3,1,2)
-contourf(x_mat, y_mat, ux_numeric_mat_total)
-colorbar;
-clim([-1,1].*max(abs(ux_mat(:))))
-axis("equal")
-colormap(parula(10))
-title(['Gauss-Legendre numerical integration of order ' num2str(N_gl)])
-set(gca,'Fontsize',12)
-
-% plot residuals as % of analytical solution
-subplot(3,1,3)
-contourf(x_mat, y_mat, 100.*(ux_mat - ux_numeric_mat_total)./ux_mat)
-cb=colorbar;cb.Label.String = '% residuals';
-clim([-1,1].*10)
-axis("equal")
-colormap(bluewhitered(40))
-title('Residuals %(analytical - numerical)')
-set(gca,'Fontsize',12)
+    % plot residuals as % of analytical solution
+    subplot(3,1,3)
+    contourf(x_mat, y_mat, 100.*(ux_mat - ux_numeric_mat_total)./ux_mat)
+    cb=colorbar;cb.Label.String = '% residuals';
+    clim([-1,1].*10)
+    axis("equal")
+    colormap(bluewhitered(40))
+    title('Residuals %(analytical - numerical)')
+    set(gca,'Fontsize',12)
+else
+    plot(x_mat,ux_mat,'-','LineWidth',4), hold on
+    plot(x_mat,ux_numeric_mat_total,'-','LineWidth',2)
+    plot([-1,1],[0,0],'k-','Linewidth',2)
+    axis tight, grid on
+    xlabel('x'), ylabel('u_x(x)')
+    % ylim([-1,1].*max(abs(ux_mat(:)))*1.1)
+    legend('analytical','GL-quadrature')
+    set(gca,'FontSize',15)
+end
 
 %% define kelvin point source function
 function [ux, sxx] = kelvin_point(x0, y0, xoffset, yoffset, fx, fy, mu, nu)
