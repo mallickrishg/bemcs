0eb411633fe8cc64278ab3f6a7177859fb2a7447
Rishav Mallick <mallickrishg@gmail.com>
2023-07-26 15:50:47 -0700

    integrating stress is just terrible

--- point_source/kelvin_integrate_numerical_2d_RM.m
+++ point_source/kelvin_integrate_numerical_2d_RM.m
@@ -26,7 +26,7 @@ fy_val = -1;
 % 1 - xy grid
 eval_type = 1;
 
-n_pts = 101;
+n_pts = 100;
 x_vec = linspace(-2, 2, n_pts);
 y_vec = linspace(-1.5, 1.5, n_pts);
 
@@ -42,15 +42,19 @@ end
 n_eval = 4; % must be an even number
 
 rectangle_x = 2*ones(n_eval,1);
-rectangle_y = logspace(-2,0,n_eval)';
+rectangle_y = logspace(-1,0,n_eval)';
 
 %% numerical integration (with matlab integral)
 
 ux_vals = zeros(length(x_mat(:,1)),length(x_mat(1,:)),n_eval);
 uy_vals = zeros(length(x_mat(:,1)),length(x_mat(1,:)),n_eval);
+sxx_vals = zeros(length(x_mat(:,1)),length(x_mat(1,:)),n_eval);
+syy_vals = zeros(length(x_mat(:,1)),length(x_mat(1,:)),n_eval);
 
 ux_numeric_int = zeros(size(x_mat));
 uy_numeric_int = zeros(size(x_mat));
+sxx_numeric_int = zeros(size(x_mat));
+syy_numeric_int = zeros(size(x_mat));
 
 tic
 for i = 1:n_eval
@@ -58,23 +62,31 @@ for i = 1:n_eval
     Rx = rectangle_x(i);
     Ry = rectangle_y(i);
     parfor k=1:numel(x_mat)
-        fun_x = @(x0,y0) gf_x(x_mat(k),y_mat(k),x0, y0, fx_val, fy_val, mu_val, nu_val);
-        fun_y = @(x0,y0) gf_y(x_mat(k),y_mat(k),x0, y0, fx_val, fy_val, mu_val, nu_val);
-                
-        ux_numeric_int(k) = integral2(fun_x,-Rx/2,Rx/2,-Ry/2,Ry/2)./area_source;
-        uy_numeric_int(k) = integral2(fun_y,-Rx/2,Rx/2,-Ry/2,Ry/2)./area_source;
+        fun_ux = @(x0,y0) gf_ux(x_mat(k),y_mat(k),x0, y0, fx_val, fy_val, mu_val, nu_val);
+        fun_uy = @(x0,y0) gf_uy(x_mat(k),y_mat(k),x0, y0, fx_val, fy_val, mu_val, nu_val);
+        fun_sxx = @(x0,y0) gf_sxx(x_mat(k),y_mat(k),x0, y0, fx_val, fy_val, mu_val, nu_val);
+        fun_syy = @(x0,y0) gf_syy(x_mat(k),y_mat(k),x0, y0, fx_val, fy_val, mu_val, nu_val);
+
+        ux_numeric_int(k) = integral2(fun_ux,-Rx/2,Rx/2,-Ry/2,Ry/2)./area_source;
+        uy_numeric_int(k) = integral2(fun_uy,-Rx/2,Rx/2,-Ry/2,Ry/2)./area_source;
+        % sxx_numeric_int(k) = integral2(fun_sxx,-Rx/2,Rx/2,-Ry/2,Ry/2)./area_source;
+        % syy_numeric_int(k) = integral2(fun_syy,-Rx/2,Rx/2,-Ry/2,Ry/2)./area_source;
     end
 
     ux_vals(:,:,i) = ux_numeric_int;
     uy_vals(:,:,i) = uy_numeric_int;
+    % sxx_vals(:,:,i) = sxx_numeric_int;
+    % syy_vals(:,:,i) = syy_numeric_int;
 end
 toc
 
 
 %% plot solutions
 figure(11),clf
+figure(12),clf
 if eval_type==1
     for i = 1:n_eval
+        figure(11)
         subplot(n_eval/2,2,i)
         n_skip = 23;
         
@@ -89,6 +101,19 @@ if eval_type==1
         axis("equal")
         title(['Source area = ' num2str(rectangle_x(i)*rectangle_y(i))])
         colormap(sky(10))
+        xlabel('x'), ylabel('y')
+        set(gca,'Fontsize',15)
+
+        figure(12)
+        subplot(n_eval/2,2,i)
+
+        toplot_n = squeeze(sxx_vals(:,:,i));
+        contourf(x_mat, y_mat, toplot_n,5), hold on
+        cb=colorbar;cb.Label.String = '\sigma_{xx}';
+        clim([-1,1].*0.02)
+        axis("equal")
+        title(['Source area = ' num2str(rectangle_x(i)*rectangle_y(i))])
+        colormap(parula(100))
         xlabel('x'), ylabel('y')
         set(gca,'Fontsize',15)
     end
@@ -112,7 +137,7 @@ else
 end
 
 % solution from last run only
-figure(12),clf
+figure(21),clf
 if eval_type==1
     n_skip = 13;
     toplot_n = sqrt(ux_numeric_int.^2 + uy_numeric_int.^2);
@@ -139,14 +164,23 @@ else
 end
 
 %% define kelvin point source function
-function ux = gf_x(x0, y0, xoffset, yoffset, fx, fy, mu, nu)
-[ux, ~] = kelvin_point(x0, y0, xoffset, yoffset, fx, fy, mu, nu);
+function ux = gf_ux(x0, y0, xoffset, yoffset, fx, fy, mu, nu)
+[ux, ~, ~, ~, ~] = kelvin_point(x0, y0, xoffset, yoffset, fx, fy, mu, nu);
 end
-function uy = gf_y(x0, y0, xoffset, yoffset, fx, fy, mu, nu)
-[~, uy] = kelvin_point(x0, y0, xoffset, yoffset, fx, fy, mu, nu);
+function uy = gf_uy(x0, y0, xoffset, yoffset, fx, fy, mu, nu)
+[~, uy, ~, ~, ~] = kelvin_point(x0, y0, xoffset, yoffset, fx, fy, mu, nu);
+end
+function sxx = gf_sxx(x0, y0, xoffset, yoffset, fx, fy, mu, nu)
+[~, ~, sxx, ~, ~] = kelvin_point(x0, y0, xoffset, yoffset, fx, fy, mu, nu);
+end
+function syy = gf_syy(x0, y0, xoffset, yoffset, fx, fy, mu, nu)
+[~, ~, ~, syy, ~] = kelvin_point(x0, y0, xoffset, yoffset, fx, fy, mu, nu);
+end
+function sxy = gf_sxy(x0, y0, xoffset, yoffset, fx, fy, mu, nu)
+[~, ~, ~, ~, sxy] = kelvin_point(x0, y0, xoffset, yoffset, fx, fy, mu, nu);
 end
 
-function [ux, uy] = kelvin_point(x0, y0, xoffset, yoffset, fx, fy, mu, nu)
+function [ux, uy, sxx, syy, sxy] = kelvin_point(x0, y0, xoffset, yoffset, fx, fy, mu, nu)
     x = x0 - xoffset;
     y = y0 - yoffset;
     C = 1 / (4 * pi * (1 - nu));
@@ -154,6 +188,12 @@ function [ux, uy] = kelvin_point(x0, y0, xoffset, yoffset, fx, fy, mu, nu)
     g = -C .* log(r);
     gx = -C .* x ./ (x.^2 + y.^2);
     gy = -C .* y ./ (x.^2 + y.^2);
+    gxy = C .* 2 .* x .* y ./ (x.^2 + y.^2).^2;
+    gxx = C .* (x.^2 - y.^2) ./ (x.^2 + y.^2).^2;
+    gyy = -gxx;
     ux = fx / (2 * mu) * ((3 - 4 * nu) .* g - x .* gx) + fy / (2 * mu) .* (-y .* gx);
     uy = fx / (2 * mu) * (-x .* gy) + fy / (2 * mu) * ((3 - 4 * nu) .* g - y .* gy);
+    sxx = fx .* (2 * (1 - nu) .* gx - x .* gxx) + fy .* (2 * nu .* gy - y .* gxx);
+    syy = fx .* (2 * nu .* gx - x .* gyy) + fy .* (2 * (1 - nu) .* gy - y .* gyy);
+    sxy = fx .* ((1 - 2 * nu) .* gy - x .* gxy) + fy .* ((1 - 2 * nu) .* gx - y .* gxy);
 end
