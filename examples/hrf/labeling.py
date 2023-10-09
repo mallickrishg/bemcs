8ff17cfcdb3b920a152a6f0080763b9d39a37cbb
Rishav Mallick <mallickrishg@gmail.com>
2023-10-09 16:50:32 -0700

    labeling and operator constructions works correctly

--- examples/hrf/labeling.py
+++ examples/hrf/labeling.py
@@ -1,8 +1,15 @@
 import numpy as np
+import bemcs
 
 
 # Function to label open nodes, overlapping interior nodes and triple junctions automatically
 def label_nodes(els):
+    """provide a dictionary of line segments using bemcs.standardize_els_geometry(els),
+    to return labelled indices for open nodes, overlapping nodes and triple junctions.
+
+    the indices are provided as a number 3(i-1)<= id <=(3i-1),
+    where 'i' corresponds to the mesh element
+    """
     n_els = len(els.x1)
     # first find all unique points
     points = np.zeros((2 * n_els, 2))
@@ -52,74 +59,99 @@ def label_nodes(els):
     return index_matrix1, index_matrix2, index_matrix3
 
 
-# Linear operator for open nodes
-for i in range(int(N_o / 2)):
-    id1 = index_open[i]  # node number
-    matrix_system_o[2 * i, :] = matrix_slip[2 * id1, :]  # x component
-    matrix_system_o[2 * i + 1, :] = matrix_slip[2 * id1 + 1, :]  # y component
+def construct_smoothoperator(els, index_open, index_overlap, index_triple):
+    """function to construct linear operator that enforces
+    continuity and smoothness conditions at non-central nodes
+    """
 
-# Linear operator for overlapping nodes
-for i in range(int(N_i / 4)):
-    idvals = index_overlap[i]  # node number
-    # continuity condition
-    matrix_system_i[4 * i, :] = (
-        matrix_slip[2 * idvals[0], :] - matrix_slip[2 * idvals[1], :]
-    )  # x
-    matrix_system_i[4 * i + 1, :] = (
-        matrix_slip[2 * idvals[0] + 1, :] - matrix_slip[2 * idvals[1] + 1, :]
-    )  # y
-    # smoothing constraints
-    matrix_system_i[4 * i + 2, :] = (
-        matrix_slip_gradient[2 * idvals[0], :] - matrix_slip_gradient[2 * idvals[1], :]
-    )  # x
-    matrix_system_i[4 * i + 3, :] = (
-        matrix_slip_gradient[2 * idvals[0] + 1, :]
-        - matrix_slip_gradient[2 * idvals[1] + 1, :]
-    )  # y
+    n_els = len(els.x1)
+    Nunknowns = 6 * n_els
+    # Design matrices (in x,y coordinates) for slip and slip gradients at each 3qn
+    matrix_slip, matrix_slip_gradient = bemcs.get_matrices_slip_slip_gradient(els)
 
-# Linear operator for triple junction nodes
-for i in range(int(N_t / 6)):
-    id1 = index_triple[i]
-    idvals = np.abs(id1)
+    N_o = 2 * len(index_open)  # open node equations
+    N_i = 4 * len(index_overlap)  # overlapping node equations
+    N_t = 6 * len(index_triple)  # triple junction equations
 
-    id_neg = idvals[id1 < 0]  # node number that need to be subtracted in TJ kinematics
-    id_pos = idvals[id1 >= 0]  # node numbers that need to be added
-    # triple junction kinematics equations
-    if len(id_neg) == 2:
-        matrix_system_t[6 * i, :] = (
-            matrix_slip[2 * id_pos, :]
-            - matrix_slip[2 * id_neg[0], :]
-            - matrix_slip[2 * id_neg[1], :]
-        )  # x component
-        matrix_system_t[6 * i + 1, :] = (
-            matrix_slip[2 * id_pos + 1, :]
-            - matrix_slip[2 * id_neg[0] + 1, :]
-            - matrix_slip[2 * id_neg[1] + 1, :]
-        )  # y component
-    else:
-        matrix_system_t[6 * i, :] = (
-            matrix_slip[2 * id_pos[0], :]
-            + matrix_slip[2 * id_pos[1], :]
-            - matrix_slip[2 * id_neg, :]
-        )  # x component
-        matrix_system_t[6 * i + 1, :] = (
-            matrix_slip[2 * id_pos[0] + 1, :]
-            + matrix_slip[2 * id_pos[1] + 1, :]
-            - matrix_slip[2 * id_neg + 1, :]
-        )  # y component
+    matrix_system_o = np.zeros((N_o, Nunknowns))
+    matrix_system_i = np.zeros((N_i, Nunknowns))
+    matrix_system_t = np.zeros((N_t, Nunknowns))
 
-    # smoothing constraints (2 nodes at a time)
-    matrix_system_t[6 * i + 2, :] = (
-        matrix_slip_gradient[2 * idvals[0], :] - matrix_slip_gradient[2 * idvals[1], :]
-    )  # x
-    matrix_system_t[6 * i + 3, :] = (
-        matrix_slip_gradient[2 * idvals[0] + 1, :]
-        - matrix_slip_gradient[2 * idvals[1] + 1, :]
-    )  # y
-    matrix_system_t[6 * i + 4, :] = (
-        matrix_slip_gradient[2 * idvals[0], :] - matrix_slip_gradient[2 * idvals[2], :]
-    )  # x
-    matrix_system_t[6 * i + 5, :] = (
-        matrix_slip_gradient[2 * idvals[0] + 1, :]
-        - matrix_slip_gradient[2 * idvals[2] + 1, :]
-    )  # y
+    # Linear operator for open nodes
+    for i in range(int(N_o / 2)):
+        id1 = index_open[i]  # node number
+        matrix_system_o[2 * i, :] = matrix_slip[2 * id1, :]  # x component
+        matrix_system_o[2 * i + 1, :] = matrix_slip[2 * id1 + 1, :]  # y component
+
+    # Linear operator for overlapping nodes
+    for i in range(int(N_i / 4)):
+        idvals = index_overlap[i]  # node number
+        # continuity condition
+        matrix_system_i[4 * i, :] = (
+            matrix_slip[2 * idvals[0], :] - matrix_slip[2 * idvals[1], :]
+        )  # x
+        matrix_system_i[4 * i + 1, :] = (
+            matrix_slip[2 * idvals[0] + 1, :] - matrix_slip[2 * idvals[1] + 1, :]
+        )  # y
+        # smoothing constraints
+        matrix_system_i[4 * i + 2, :] = (
+            matrix_slip_gradient[2 * idvals[0], :]
+            - matrix_slip_gradient[2 * idvals[1], :]
+        )  # x
+        matrix_system_i[4 * i + 3, :] = (
+            matrix_slip_gradient[2 * idvals[0] + 1, :]
+            - matrix_slip_gradient[2 * idvals[1] + 1, :]
+        )  # y
+
+    # Linear operator for triple junction nodes
+    for k in range(int(N_t / 6)):
+        id1 = index_triple[k]
+        idvalst = np.abs(id1)
+
+        # node number that need to be subtracted in TJ kinematics
+        id_neg = idvalst[id1 < 0]
+        # node numbers that need to be added
+        id_pos = idvalst[id1 >= 0]
+        # triple junction kinematics equations
+        if len(id_neg) == 2:
+            matrix_system_t[6 * k, :] = (
+                matrix_slip[2 * id_pos, :]
+                - matrix_slip[2 * id_neg[0], :]
+                - matrix_slip[2 * id_neg[1], :]
+            )  # x component
+            matrix_system_t[6 * k + 1, :] = (
+                matrix_slip[2 * id_pos + 1, :]
+                - matrix_slip[2 * id_neg[0] + 1, :]
+                - matrix_slip[2 * id_neg[1] + 1, :]
+            )  # y component
+        else:
+            matrix_system_t[6 * k, :] = (
+                matrix_slip[2 * id_pos[0], :]
+                + matrix_slip[2 * id_pos[1], :]
+                - matrix_slip[2 * id_neg, :]
+            )  # x component
+            matrix_system_t[6 * k + 1, :] = (
+                matrix_slip[2 * id_pos[0] + 1, :]
+                + matrix_slip[2 * id_pos[1] + 1, :]
+                - matrix_slip[2 * id_neg + 1, :]
+            )  # y component
+
+        # smoothing constraints (2 nodes at a time)
+        matrix_system_t[6 * k + 2, :] = (
+            matrix_slip_gradient[2 * idvalst[0], :]
+            - matrix_slip_gradient[2 * idvalst[1], :]
+        )  # x
+        matrix_system_t[6 * k + 3, :] = (
+            matrix_slip_gradient[2 * idvalst[0] + 1, :]
+            - matrix_slip_gradient[2 * idvalst[1] + 1, :]
+        )  # y
+        matrix_system_t[6 * k + 4, :] = (
+            matrix_slip_gradient[2 * idvalst[0], :]
+            - matrix_slip_gradient[2 * idvalst[2], :]
+        )  # x
+        matrix_system_t[6 * k + 5, :] = (
+            matrix_slip_gradient[2 * idvalst[0] + 1, :]
+            - matrix_slip_gradient[2 * idvalst[2] + 1, :]
+        )  # y
+
+    return matrix_system_o, matrix_system_i, matrix_system_t
