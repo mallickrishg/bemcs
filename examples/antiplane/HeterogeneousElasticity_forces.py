6254aa90fc2dd76ff30ff6b6cf7610254efeff74
Rishav Mallick <mallickrishg@gmail.com>
2025-10-17 17:07:15 +0800

    fixing errors in function calls

--- examples/antiplane/HeterogeneousElasticity_forces.py
+++ examples/antiplane/HeterogeneousElasticity_forces.py
@@ -4,7 +4,10 @@ import numpy as np
 import bemcs
 import pandas as pd
 import GF
+import warnings
 
+# Suppress all warnings
+warnings.filterwarnings("ignore")
 # %% Read source file and mesh of the domain
 fileinput = "HeterogeneousDomainMesh.csv"
 connectvitiyfile = "HeterogeneousDomainMeshConnectivity.csv"
@@ -32,7 +35,7 @@ els.y1 = y1
 els.x2 = x2
 els.y2 = y2
 
-# construct separate mesh just for 's' or 't' BCtype
+# construct separate mesh just for 's', 'u' or 't' BCtype
 bc_mask = BCtype != "h"
 els_s = bemcs.initialize_els()
 els_s.x1 = els.x1[bc_mask]
@@ -55,7 +58,7 @@ bemcs.plot_els_geometry(els)
 
 # Define observation points
 # need to shift observation points by an infinitesimal amount to sample discontinuity either in u or du/dn
-dr = -1e-6
+dr = -1e-9
 
 xo = np.hstack(
     (
@@ -75,10 +78,36 @@ yo = np.hstack(
 # %% construct kernels
 # compute force kernels (assuming trapezoidal spatial functions) for "h" BCtype elements [Nobs x Nsources]
 K_x, K_y, _ = GF.get_kernels_trapezoidalforce(xo, yo, els, connmatrix)
-
-# compute slip kernels for "s" & "t" BCtype elements (will be a square matrix of dimension N = stride x N_slip_elements)
-matrix_slip, BC_slip = GF.construct_linearoperator_slip(
-    els_s, BCtype[bc_mask], BCval[bc_mask]
+# compute traction kernel by dotting stress with normal vector
+nxvec = np.hstack(
+    (
+        els_s.x_normals,
+        els.x_normals[connmatrix[:, 1].astype(int)],
+    )
+)
+nyvec = np.hstack(
+    (
+        els_s.y_normals,
+        els.y_normals[connmatrix[:, 1].astype(int)],
+    )
 )
 
-# Assemble BIE matrices for slip & force kernels and appropriate Boundary Conditions
+# convert from stress to traction kernels [Nobs x Nsources]
+K_n = K_x * nxvec[:, None] + K_y * nyvec[:, None]
+
+# compute slip kernels for "s" & "t" BCtype elements (will be a square matrix of dimension N = stride x N_slip_elements)
+matrix_slip_c, matrix_slip_nodes, BC_slip_c, BC_slip_nodes = (
+    GF.construct_linearoperator_slip(els_s, BCtype[bc_mask], BCval[bc_mask])
+)
+
+plt.figure(figsize=(8, 8))
+plt.imshow(K_n, cmap="seismic")
+plt.colorbar()
+plt.clim([-0.5, 0.5])
+plt.show()
+plt.figure(figsize=(8, 8))
+plt.imshow(np.vstack((matrix_slip_c, matrix_slip_nodes)), cmap="seismic")
+plt.colorbar()
+plt.clim([-0.2, 0.2])
+plt.show()
+# %% Assemble BIE matrices for slip & force kernels and appropriate Boundary Conditions
