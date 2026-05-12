0b844dbce8cc151a7acc4d73ab01ac2b8f6a5c6f
Rishav Mallick <mallickrishg@gmail.com>
2026-05-12 14:09:28 +0800

    tested computed displacements, stresses and a hack for slip on fault for the new hat-shaped slip kernels

--- examples/testing_QuadraticHatSlipPlanestrain.py
+++ examples/testing_QuadraticHatSlipPlanestrain.py
@@ -1,4 +1,4 @@
-#%%
+# %%
 import numpy as np
 import bemcs
 import bemcs.bemAssembly as GF
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import warnings
 
-#%%
+# %%
 # Load mesh
 fileinput = "dummy_mesh.csv"
 datain = pd.read_csv(fileinput)
@@ -25,7 +25,7 @@ bemcs.plot_els_geometry(els)
 connect_matrix = np.array([[2, 1, 0], [1, 0, 3], [0, 3, 4]])
 
 coefs_s, coefs_n = GF.compute_coefs_quadratichatslip_planestrain(els, connect_matrix)
-    
+
 print("Shape of coefs_s:", coefs_s.shape)
 print("Shape of coefs_n:", coefs_n.shape)
 
@@ -33,7 +33,7 @@ print("Shape of coefs_n:", coefs_n.shape)
 n_obs = 100
 width = 3.0
 x_obs = np.linspace(-width, width, n_obs)
-y_obs = np.linspace(-width/2, width/2, n_obs)
+y_obs = np.linspace(-width / 2, width / 2, n_obs)
 x_obs, y_obs = np.meshgrid(x_obs, y_obs)
 x_obs = x_obs.flatten()
 y_obs = y_obs.flatten()
@@ -48,12 +48,8 @@ for i in range(len(els.x1)):
     quadratic_coefs_s[3 * i : 3 * (i + 1)] = coefs_s[i, :3, patch_index]
     quadratic_coefs_n[3 * i : 3 * (i + 1)] = coefs_s[i, 3:, patch_index]
 
-kernels_s = bemcs.get_displacement_stress_kernel(
-    x_obs, y_obs, els, mu, nu, "shear"
-)
-kernels_n = bemcs.get_displacement_stress_kernel(
-    x_obs, y_obs, els, mu, nu, "normal"
-)
+kernels_s = bemcs.get_displacement_stress_kernel(x_obs, y_obs, els, mu, nu, "shear")
+kernels_n = bemcs.get_displacement_stress_kernel(x_obs, y_obs, els, mu, nu, "normal")
 
 ux, uy, sxx, syy, sxy = bemcs.coeffs_to_disp_stress(
     kernels_s, kernels_n, quadratic_coefs_s, quadratic_coefs_n
@@ -74,10 +70,11 @@ bemcs.plot_displacements_stresses_els(
 
 # Test with the combined kernels for all patches
 K_ux, K_uy, K_sxx, K_syy, K_sxy = GF.get_kernels_quadratichatslip_planestrain(
-    x_obs, y_obs, els, connect_matrix, mu, nu)
-slip = np.zeros((len(connect_matrix[:, 0])*2, 1))  # zero slip for all elements
-slip[4,0] = 1
-slip[0,0] = 1
+    x_obs, y_obs, els, connect_matrix, mu, nu
+)
+slip = np.zeros((len(connect_matrix[:, 0]) * 2, 1))  # zero slip for all elements
+slip[4, 0] = 1
+slip[0, 0] = 0
 # compute ux,uy,sxx,syy,sxy for this slip distribution
 ux = K_ux @ slip
 uy = K_uy @ slip
@@ -97,3 +94,26 @@ bemcs.plot_displacements_stresses_els(
     y_obs,
     n_skip_plot=31,
 )
+
+# %% compute slip
+# Most importantly return slip at the center of each mesh element.
+dr = 1e-9
+K_ux_p, K_uy_p, _, _, _ = GF.get_kernels_quadratichatslip_planestrain(
+    els.x_centers + dr * els.x_normals,
+    els.y_centers + dr * els.y_normals,
+    els,
+    connect_matrix,
+    mu,
+    nu,
+)
+K_ux_m, K_uy_m, _, _, _ = GF.get_kernels_quadratichatslip_planestrain(
+    els.x_centers - dr * els.x_normals,
+    els.y_centers - dr * els.y_normals,
+    els,
+    connect_matrix,
+    mu,
+    nu,
+)
+K_sx = K_ux_p - K_ux_m
+K_sy = K_uy_p - K_uy_m
+# %%
